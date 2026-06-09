1e41ea9485124b5ac880c4af3b87621f26bf9279
joaords98-ctrl <joaords98@gmail.com>
2026-06-08 21:45:31 -0300

    Delete App.jsx

--- App.jsx
+++
@@ -1,7 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-  <rect width="100" height="100" rx="20" fill="#0E3B2E"/>
-  <circle cx="50" cy="50" r="38" fill="none" stroke="#5fd6ac" stroke-width="3"/>
-  <circle cx="50" cy="35" r="6" fill="#C0392B"/>
-  <path d="M32 58 Q41 50 50 56 T70 51" fill="none" stroke="#1D9E75" stroke-width="6" stroke-linecap="round"/>
-  <path d="M32 66 Q41 60 50 64 T68 61" fill="none" stroke="#7d9b8f" stroke-width="6" stroke-linecap="round"/>
-</svg>
